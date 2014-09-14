ec933c7c3551a4a074310c484171e66ff67482f0
HaiYiMao <harrymao@Harrys-MacBook-Pro.local>
2014-09-14 12:53:29 -0400

    12.rkt first time committing

---
+++ 12.rkt
@@ -0,0 +1,44 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |12|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define-struct point (x, y))
+;; A Point is a
+;;(make-point Number Number)
+;;Interpretation
+;;x is the value of X-coordination
+;;Y is the value of Y-coordination
+;;point-fn: Point->??
+#|(define(point-fn p)
+  (...
+   (point-x p)
+   (point-y p)
+   (point? p))
+|#
+;;point-x:Point->Number
+;;GIVEN:  a point 
+;;RETURNS: the value of the X coordination
+
+  
+;;point-y:Point->Number
+;;GIVEN:  a point
+;;RETURNS:the value of the Y coordination
+  
+;;point? :?->Boolean
+;;GIVEN:  
+;;RETURNS: true when the input is point
+;;Examples:
+;;(make-point 5 3)
+;;(point? 5)=false
+;;(point? true)=false
+;;(point?(make-point 2 1))=true
+;;(point-x(make-point 8 5))=8
+;;(point-y(make-point 42 15))=15
+
+
+
+(make-point 5 3)
+(point? 5)=false
+(point? true)=false
+(point?(make-point 2 1))=true
+(point-x(make-point 8 5))=8
+(point-y(make-point 42 15))=15
