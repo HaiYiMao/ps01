890994c1d9a36248991898b04f7e0c71c5e91ff2
HaiYiMao <harrymao@Harrys-MacBook-Pro.local>
2014-09-15 15:37:45 -0400

    changes in 31

--- 31.rkt
+++ 31.rkt
@@ -1,16 +1,400 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |31|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-(require 2htdp/image)
-;;draw-list-circles: List of PosNumber-> cirles
-;;PURPOSE
-;;GIVEN:a List of Numbers
-;;which are the radium of circles
-;;RETURN:a series of circle with given radiums
+#reader(lib"read.ss""wxme")WXME0108 ## 
+#|
+   This file uses the GRacket editor format.
+   Open this file in DrRacket version 6.1 or later to read it.
 
-(define (draw-list-circles  ls)
-  (cond
-  [(empty? ls) (circle 0 "outline" "white")]
-  [else(overlay (circle (first ls) "outline" "olive") (draw-list-circles(rest ls )))]
- ))
-(draw-list-circles (list 10 20 30 40 50 60 70 80 90 ) )
+   Most likely, it was created by saving a program in DrRacket,
+   and it probably contains a program with non-text elements
+   (such as images or comment boxes).
+
+            http://racket-lang.org/
+|#
+ 31 7 #"wxtext\0"
+3 1 6 #"wxtab\0"
+1 1 8 #"wximage\0"
+2 0 8 #"wxmedia\0"
+4 1 34 #"(lib \"syntax-browser.ss\" \"mrlib\")\0"
+1 0 16 #"drscheme:number\0"
+3 0 44 #"(lib \"number-snip.ss\" \"drscheme\" \"private\")\0"
+1 0 36 #"(lib \"comment-snip.ss\" \"framework\")\0"
+1 0 93
+(
+ #"((lib \"collapsed-snipclass.ss\" \"framework\") (lib \"collapsed-sni"
+ #"pclass-wxme.ss\" \"framework\"))\0"
+) 0 0 43 #"(lib \"collapsed-snipclass.ss\" \"framework\")\0"
+0 0 19 #"drscheme:sexp-snip\0"
+0 0 36 #"(lib \"cache-image-snip.ss\" \"mrlib\")\0"
+1 0 68
+(
+ #"((lib \"image-core.ss\" \"mrlib\") (lib \"image-core-wxme.rkt\" \"mr"
+ #"lib\"))\0"
+) 1 0 29 #"drscheme:bindings-snipclass%\0"
+1 0 88
+(
+ #"((lib \"pict-snip.rkt\" \"drracket\" \"private\") (lib \"pict-snip.r"
+ #"kt\" \"drracket\" \"private\"))\0"
+) 0 0 34 #"(lib \"bullet-snip.rkt\" \"browser\")\0"
+0 0 25 #"(lib \"matrix.ss\" \"htdp\")\0"
+1 0 22 #"drscheme:lambda-snip%\0"
+1 0 29 #"drclickable-string-snipclass\0"
+0 0 26 #"drracket:spacer-snipclass\0"
+0 0 57
+#"(lib \"hrule-snip.rkt\" \"macro-debugger\" \"syntax-browser\")\0"
+1 0 26 #"drscheme:pict-value-snip%\0"
+0 0 45 #"(lib \"image-snipr.ss\" \"slideshow\" \"private\")\0"
+1 0 38 #"(lib \"pict-snipclass.ss\" \"slideshow\")\0"
+2 0 55 #"(lib \"vertical-separator-snip.ss\" \"stepper\" \"private\")\0"
+1 0 18 #"drscheme:xml-snip\0"
+1 0 31 #"(lib \"xml-snipclass.ss\" \"xml\")\0"
+1 0 21 #"drscheme:scheme-snip\0"
+2 0 34 #"(lib \"scheme-snipclass.ss\" \"xml\")\0"
+1 0 10 #"text-box%\0"
+1 0 32 #"(lib \"text-snipclass.ss\" \"xml\")\0"
+1 0 1 6 #"wxloc\0"
+          0 0 60 0 1 #"\0"
+0 75 1 #"\0"
+0 12 90 -1 90 -1 3 -1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 255 255 255 1 -1 0 9
+#"Standard\0"
+0 75 6 #"Menlo\0"
+0 12 90 -1 90 -1 3 -1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 255 255 255 1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 -1 -1 2 24
+#"framework:default-color\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1 2
+1 #"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 150 0 150 0 0 0 -1 -1 2 15
+#"text:ports out\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 150 0 150 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 93 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 255 0 0 0 0 0 -1
+-1 2 15 #"text:ports err\0"
+0 -1 1 #"\0"
+1 0 -1 92 93 -1 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 175 0 0 0 -1 -1 2 17
+#"text:ports value\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 175 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1.0 0 92 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 34 139 34 0 0 0 -1
+-1 2 27 #"Matching Parenthesis Style\0"
+0 -1 1 #"\0"
+1.0 0 92 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 34 139 34 0 0 0 -1
+-1 2 1 #"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 37
+#"framework:syntax-color:scheme:symbol\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 38
+#"framework:syntax-color:scheme:keyword\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 38 38 128 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 194 116 31 0 0 0 -1 -1 2
+38 #"framework:syntax-color:scheme:comment\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 194 116 31 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 37
+#"framework:syntax-color:scheme:string\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 35
+#"framework:syntax-color:scheme:text\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 39
+#"framework:syntax-color:scheme:constant\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 41 128 38 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 49
+#"framework:syntax-color:scheme:hash-colon-keyword\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 42
+#"framework:syntax-color:scheme:parenthesis\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 132 60 36 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 36
+#"framework:syntax-color:scheme:error\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 36
+#"framework:syntax-color:scheme:other\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 16
+#"Misspelled Text\0"
+0 -1 1 #"\0"
+1 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 81 112 203 0 0 0 -1 -1 2
+38 #"drracket:check-syntax:lexically-bound\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 81 112 203 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 28
+#"drracket:check-syntax:set!d\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 37
+#"drracket:check-syntax:unused-require\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 36
+#"drracket:check-syntax:free-variable\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 255 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 68 0 203 0 0 0 -1 -1 2 31
+#"drracket:check-syntax:imported\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 68 0 203 0 0 0 -1 -1 2 47
+#"drracket:check-syntax:my-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 178 34 34 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 116 0 0 0 0 -1 -1 2 50
+#"drracket:check-syntax:their-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 116 0 0 0 0 -1 -1 2 48
+#"drracket:check-syntax:unk-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 139 142 28 0 0 0 -1 -1 2
+49 #"drracket:check-syntax:both-obligation-style-pref\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 139 142 28 0 0 0 -1 -1 2
+26 #"plt:htdp:test-coverage-on\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 1
+#"\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 2 27
+#"plt:htdp:test-coverage-off\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 4 1
+#"\0"
+0 70 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 4 4 #"XML\0"
+0 70 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 2 37 #"plt:module-language:test-coverage-on\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 -1 -1 2 38
+#"plt:module-language:test-coverage-off\0"
+0 -1 1 #"\0"
+1 0 -1 92 -1 93 -1 -1 0 1 0 0 0 1 0 0 0 0 0 0 255 165 0 0 0 0 -1 -1 4 1
+#"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 1.0 1.0 1.0 1.0 1.0 1.0 0 0 0 0 0 0
+-1 -1 4 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 1 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 0 255 0 0 0 -1
+-1 4 1 #"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 1 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 0 255 0 0 0 -1
+-1 4 1 #"\0"
+0 71 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 1.0 1.0 1.0 0 100 0 0 0 0 -1
+-1 17 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1
+24 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1
+44 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1
+46 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1
+4 1 #"\0"
+0 -1 1 #"\0"
+1.0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 255 255 255 -1 -1
+          0 125 0 4 3 85
+(
+ #";; The first three lines of this file were inserted by DrRacket. The"
+ #"y record metadata"
+) 0 0 4 29 1 #"\n"
+0 0 4 3 85
+(
+ #";; about the language level of this file in a form that our tools ca"
+ #"n easily process."
+) 0 0 4 29 1 #"\n"
+0 0 4 3 171
+(
+ #"#reader(lib \"htdp-beginner-reader.ss\" \"lang\")((modname |31|) (re"
+ #"ad-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructo"
+ #"r repeating-decimal #f #t none #f ())))"
+) 0 0 4 29 1 #"\n"
+0 0 24 3 1 #"("
+0 0 15 3 7 #"require"
+0 0 24 3 1 #" "
+0 0 14 3 11 #"2htdp/image"
+0 0 24 3 1 #")"
+0 0 24 29 1 #"\n"
+0 0 17 3 23 #";;DATA DEFINITIONS:none"
+0 0 24 29 1 #"\n"
+0 0 24 29 1 #"\n"
+0 0 17 3 47 #";;draw-list-circles: List of PosNumber-> cirles"
+0 0 24 29 1 #"\n"
+0 0 17 3 9 #";;PURPOSE"
+0 0 24 29 1 #"\n"
+0 0 17 3 25 #";;GIVEN:a List of Numbers"
+0 0 24 29 1 #"\n"
+0 0 17 3 33 #";;which are the radium of circles"
+0 0 24 29 1 #"\n"
+0 0 17 3 46 #";;RETURN:a series of circle with given radiums"
+0 0 24 29 1 #"\n"
+0 0 17 3 12 #";; EXAMPLES:"
+0 0 17 3 1 #"("
+0 0 17 3 17 #"draw-list-circles"
+0 0 17 3 2 #" ("
+0 0 17 3 4 #"list"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"10"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"20"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"30"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"40"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"50"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"60"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"70"
+0 0 17 3 1 #" "
+0 0 17 3 2 #"80"
+0 0 17 3 1 #" "
+0 0 17 3 7 #"90 ) )="
+0 0 24 29 1 #"\n"
+0 0 17 3 2 #";;"
+0 12           4 17 1327
+(
+ #"(#(struct:overlay #(struct:translate 80 80 #(struct:translate 10 10 "
+ #"#(struct:ellipse 20 20 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 70 70 #(struct:translate 20 20 "
+ #"#(struct:ellipse 40 40 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 60 60 #(struct:translate 30 30 "
+ #"#(struct:ellipse 60 60 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 50 50 #(struct:translate 40 40 "
+ #"#(struct:ellipse 80 80 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 40 40 #(struct:translate 50 50 "
+ #"#(struct:ellipse 100 100 0 outline \"olive\"))) #(struct:translate 0"
+ #" 0 #(struct:overlay #(struct:translate 30 30 #(struct:translate 60 6"
+ #"0 #(struct:ellipse 120 120 0 outline \"olive\"))) #(struct:translate"
+ #" 0 0 #(struct:overlay #(struct:translate 20 20 #(struct:translate 70"
+ #" 70 #(struct:ellipse 140 140 0 outline \"olive\"))) #(struct:transla"
+ #"te 0 0 #(struct:overlay #(struct:translate 10 10 #(struct:translate "
+ #"80 80 #(struct:ellipse 160 160 0 outline \"olive\"))) #(struct:trans"
+ #"late 0 0 #(struct:overlay #(struct:translate 0 0 #(struct:translate "
+ #"90 90 #(struct:ellipse 180 180 0 outline \"olive\"))) #(struct:trans"
+ #"late 90 90 #(struct:translate 0 0 #(struct:ellipse 0 0 0 outline \"w"
+ #"hite\")))))))))))))))))))) #(struct:bb 180 180 180) #f)"
+) 0 0 24 29 1 #"\n"
+0 0 24 29 1 #"\n"
+0 0 24 3 1 #"("
+0 0 15 3 6 #"define"
+0 0 24 3 2 #" ("
+0 0 14 3 17 #"draw-list-circles"
+0 0 24 3 2 #"  "
+0 0 14 3 2 #"ls"
+0 0 24 3 1 #")"
+0 0 24 29 1 #"\n"
+0 0 24 3 3 #"  ("
+0 0 15 3 4 #"cond"
+0 0 24 29 1 #"\n"
+0 0 24 3 4 #"  [("
+0 0 14 3 6 #"empty?"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"ls"
+0 0 24 3 3 #") ("
+0 0 14 3 6 #"circle"
+0 0 24 3 1 #" "
+0 0 21 3 1 #"0"
+0 0 24 3 1 #" "
+0 0 19 3 9 #"\"outline\""
+0 0 24 3 1 #" "
+0 0 19 3 7 #"\"white\""
+0 0 24 3 2 #")]"
+0 0 24 29 1 #"\n"
+0 0 24 3 3 #"  ["
+0 0 14 3 4 #"else"
+0 0 24 3 1 #"("
+0 0 14 3 7 #"overlay"
+0 0 24 3 2 #" ("
+0 0 14 3 6 #"circle"
+0 0 24 3 2 #" ("
+0 0 14 3 5 #"first"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"ls"
+0 0 24 3 2 #") "
+0 0 19 3 9 #"\"outline\""
+0 0 24 3 1 #" "
+0 0 19 3 7 #"\"olive\""
+0 0 24 3 3 #") ("
+0 0 14 3 17 #"draw-list-circles"
+0 0 24 3 1 #"("
+0 0 14 3 4 #"rest"
+0 0 24 3 1 #" "
+0 0 14 3 2 #"ls"
+0 0 24 3 5 #" )))]"
+0 0 24 29 1 #"\n"
+0 0 24 3 3 #" ))"
+0 0 24 29 1 #"\n"
+0 0 17 3 7 #";;TESTS"
+0 0 24 29 1 #"\n"
+0 0 24 29 1 #"\n"
+0 0 24 3 1 #"("
+0 0 14 3 17 #"draw-list-circles"
+0 0 24 3 2 #" ("
+0 0 14 3 4 #"list"
+0 0 24 3 1 #" "
+0 0 21 3 2 #"10"
+0 0 24 3 1 #" "
+0 0 21 3 2 #"20"
+0 0 24 3 1 #" "
+0 0 21 3 2 #"30"
+0 0 24 3 1 #" "
+0 0 21 3 2 #"80"
+0 0 24 3 1 #" "
+0 0 21 3 2 #"90"
+0 0 24 3 4 #" ) )"
+0 0 24 29 1 #"\n"
+0 0 17 3 23 #";; the result should be"
+0 0 24 29 1 #"\n"
+0 0 17 3 2 #";;"
+0 12           4 17 777
+(
+ #"(#(struct:overlay #(struct:translate 80 80 #(struct:translate 10 10 "
+ #"#(struct:ellipse 20 20 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 70 70 #(struct:translate 20 20 "
+ #"#(struct:ellipse 40 40 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 60 60 #(struct:translate 30 30 "
+ #"#(struct:ellipse 60 60 0 outline \"olive\"))) #(struct:translate 0 0"
+ #" #(struct:overlay #(struct:translate 10 10 #(struct:translate 80 80 "
+ #"#(struct:ellipse 160 160 0 outline \"olive\"))) #(struct:translate 0"
+ #" 0 #(struct:overlay #(struct:translate 0 0 #(struct:translate 90 90 "
+ #"#(struct:ellipse 180 180 0 outline \"olive\"))) #(struct:translate 9"
+ #"0 90 #(struct:translate 0 0 #(struct:ellipse 0 0 0 outline \"white\""
+ #")))))))))))) #(struct:bb 180 180 180) #f)"
+) 0           0
