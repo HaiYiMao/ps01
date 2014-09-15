fbe9dd4a6a33bf65fea4b17aabc40cbfbc0449fc
HaiYiMao <harrymao@Harrys-MacBook-Pro.local>
2014-09-15 19:02:05 -0400

    11

--- 11.rkt
+++ 11.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Untitled) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define-struct point (x y))
 ;; A Point is a
 ;;(make-point Number Number)
@@ -16,13 +16,13 @@
 |#
 ;;point-x:Point->Number
 ;;GIVEN:  a point 
-;;RETURNS: the value of the X coordination
+;;RETURN: the value of the X coordination
 
   
 ;;point-y:Point->Number
 ;;GIVEN:  a point
-;;RETURNS:the value of the Y coordination
+;;RETURN:the value of the Y coordination
   
 ;;point? :?->Boolean
 ;;GIVEN:  
-;;RETURNS: true when the input is point
+;;RETURN: true when the input is point
